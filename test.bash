76b3406074914c32e4cbc419a20147dbab668951
k310sto <k310.sto@gmail.com>
2024-10-31 12:35:25 +0900

    test(3 変な入力)

--- test.bash
+++ test.bash
@@ -4,10 +4,20 @@ ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
-
 res=0
+
+###普通入力###
 out=$(seq 5 | ./plus.py)
 [ "${out}" = "15.0" ] || ng "$LINENO"
+
+###変入力###
+out=$(echo あ | ./plus.py)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus.py)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit "$res"
